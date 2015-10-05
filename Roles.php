323c9916b38e18b488d111f91f025778fc45b736
Evgeniy Stepanov <zhenyast@yandex.ru>
2015-10-05 13:44:20 +0300

    Update Roles.php

--- Roles.php
+++ Roles.php
@@ -1,54 +1,72 @@
 <?php
+namespace app\controllers;
 
-namespace app\models;
+use yii\rest\ActiveController;
+use app\models\User;
+use app\models\LoginForm;
+use app\models\UserRoleView;
+use app\models\Role;
 
-use Yii;
-
-/**
- * This is the model class for table "role".
- *
- * @property integer $role_id
- * @property string $role_name
- *
- * @property User[] $users
- */
-class Roles extends \yii\db\ActiveRecord
+class UserController extends ActiveController
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'role';
-    }
+    public $modelClass = 'app\models\User';
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
+    public function actionLogin()
     {
-        return [
-            [['role_name'], 'required'],
-            [['role_name'], 'string', 'max' => 30]
-        ];
-    }
+        $modelLoginFrom = new LoginForm();
+        if ($modelLoginFrom->load(\Yii::$app->getRequest()->getBodyParams(), '') && $modelLoginFrom->login()) {
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'role_id' => 'Role ID',
-            'role_name' => 'Role Name',
-        ];
-    }
+            $modelRoleView = new UserRoleView();
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getUsers()
-    {
-        return $this->hasMany(User::className(), ['role_id' => 'role_id']);
+            $result = $modelRoleView->find()
+                ->where(['=','user_id', \Yii::$app->user->identity->getId()]
+                )->all();
+
+            $roles_table = new Role();
+            foreach($result as $key){
+                $role_name = $roles_table->find()
+                    ->where(['=','role_id', $key->role_id]
+                    )->one();
+                $user_role[] = $role_name->name;
+            }
+            return [
+                \Yii::$app->user->identity->getAuthKey(),
+                $user_role
+            ];
+        } else {
+            return $modelLoginFrom;
+        }
+    }
+    public function actionRestorepass(){
+        if (!$post = \Yii::$app->getRequest()->getBodyParams()) {
+            throw new \yii\web\HttpException(400, 'No data was posted');
+        }
+        $model = User::findByUsername($post['username']);
+        if (!$model->username){
+            throw new \yii\web\HttpException(400, 'Username is incorrect');
+        }
+        $model->generatePasswordResetToken();
+        $model->save();
+        $url = 'http://web/rest.php/users/changepass?u=' . $model->username . '&p=' . $model->password_reset_token;
+        \Yii::$app->mailer->compose()
+            ->setFrom('localhost@domain.com')
+            ->setTo('ZhenyaSt@yandex.ru')
+            ->setSubject('Restore Password')
+            ->setTextBody('')
+            ->setHtmlBody("<b>$url</b>")
+            ->send();
+    }
+    public function actionChangepass(){
+        if (!$get = \Yii::$app->getRequest()->queryParams) {
+            throw new \yii\web\HttpException(400, 'No data was posted');
+        }
+        $model = User::findByUsername($get['u']);
+        $result = $model->find()
+            ->where(['=','password_reset_token', $get['p']]
+            )->one();
+        if (!$result){
+            throw new \yii\web\HttpException(400, 'Password reset token is not valid');
+        }
+        //create new password
     }
 }
